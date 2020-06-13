65a69b80b02b8761cb74935bab0a99923a5ae1fb
Pedro <pedrolucaspancine@gmail.com>
2020-06-13 20:39:56 -0300

    Mapa centralizado no Brasil

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8
+    center: { lat: -10.183056, lng: -48.333611 },
+    zoom: 4
   });
 }
 
